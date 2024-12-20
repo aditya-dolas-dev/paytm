920a87807a3875d61e99616fd5c3f2eb384ce75c
aditya <themadtitan08@gmail.com>
2024-12-20 18:51:49 +0530

    backend commit

---
+++ backend/middlewares/userMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET}= require("../config");
+
+
+
+function userMiddleWare(req,res,next){
+
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader || !authHeader.startsWith('Bearer')){
+    res.status(403).json({msg:"invalid token"})
+  }
+  const words = authHeader.split(" ");
+  const jwtToken = words[1];
+  
+  try{
+  const decodedValue = jwt.verify(jwtToken,JWT_SECRET);
+  if(decodedValue){
+    req.userId = decodedValue.userId
+    next()
+  }
+}catch(error){
+  return res.status(403).json({})
+}
+
+}
+
+module.exports=userMiddleWare;
